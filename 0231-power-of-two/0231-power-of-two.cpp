4a588577b951b972cfdf8b4397705c54cec3a3a1
Charishma Pothireddy <cherryspl09@gmail.com>
2024-12-27 18:35:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (42.97%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    void isPowerUtil(int n,int& cnt){
-        if (n==0) return;
-        cnt+=(n & 1);
-        isPowerUtil((n>>1),cnt);
-    }
     bool isPowerOfTwo(int n) {
         if (n<=0) return false;
-        int cnt=0;
-        isPowerUtil(n,cnt);
-        return cnt==1;
+        if (n==1) return true;
+        if (n%2==0){
+            n=n/2;
+            return isPowerOfTwo(n);
+        }
+        return false;
     }
 };
