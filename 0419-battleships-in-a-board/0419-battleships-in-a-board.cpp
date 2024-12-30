431a9fc4013e8875f4842f8af9e4b0986f14c4a7
Charishma Pothireddy <cherryspl09@gmail.com>
2024-12-30 12:02:23 +0530

    Time: 0 ms (100%), Space: 12.1 MB (29.9%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,44 +1,15 @@
 class Solution {
 public:
-    int bfs(int row,int col,int numRows,int numCols,vector<vector<bool>>& visited,vector<vector<char>>& grid){
-        queue<pair<int,int>> que;
-        que.push({row,col});
-        // row check
-        bool rowCheck=false;
-        while (!que.empty()){
-            pair<int,int> node=que.front();
-            que.pop();
-            int newRow=node.first+1;
-            if (newRow>=0 && newRow<numRows && grid[newRow][node.second]=='X' && !visited[newRow][node.second]){
-                que.push({newRow,node.second});
-                visited[newRow][node.second]=true;
-                rowCheck=true;
-            }
-        }
-        if (rowCheck) return 1;
-        que.push({row,col});
-        bool colCheck=false;
-        while (!que.empty()){
-            pair<int,int> node=que.front();
-            que.pop();
-            int newCol=node.second+1;
-            if (newCol>=0 && newCol<numCols && grid[node.first][newCol]=='X' && !visited[node.first][newCol]){
-                que.push({node.first,newCol});
-                visited[node.first][newCol]=true;
-                colCheck=true;
-            }
-        }
-        return 1;
-    }
     int countBattleships(vector<vector<char>>& board) {
         int numRows=board.size();
         int numCols=board[0].size();
-        vector<vector<bool>> visited(numRows,vector<bool>(numCols,false));
         int cnt=0;
-        for (int row=0;row<numRows;row++){
-            for (int col=0;col<numCols;col++){
-                if (board[row][col]=='X' && !visited[row][col]){
-                    cnt+=bfs(row,col,numRows,numCols,visited,board);
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                if (board[i][j]=='X'){
+                    if (i-1<0 || (i-1>=0 && board[i-1][j]!='X')){
+                        if (j-1<0 || (j-1>=0 && board[i][j-1]!='X')) cnt++;
+                    }
                 }
             }
         }
